4679fb45675f27b9e97f1f66cf82e629a1435470
sirisha-indeti <indetisirisha123@gmail.com>
2026-04-30 11:20:09 +0530

    app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Register from "./pages/Register.jsx"
+import Login from "./pages/Login.jsx"
 
 export default function App() {
   return (
     <div>
-      App page
+      <BrowserRouter>
+      <Route path="/Register" element={<Register/>}/>
+      <Route path="/Login"  element={<Login/>}/>
+      </BrowserRouter>
     </div>
   )
 }
