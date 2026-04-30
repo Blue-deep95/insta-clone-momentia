c433d25982b0071681931aecfb23f41597c12647
Blue-deep95 <bluedeep95@outlook.com>
2026-04-30 14:22:32 +0530

    created schemas of comment,follow,post,user,generatetokens created db connections

---
+++ backend/models/User.js
@@ -0,0 +1,48 @@
+const mongoose =require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:String,
+
+    profilePicture:{
+        original: String,
+        profileView:String,
+        commentView:String
+    },
+    blockedUsers:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user"
+    }],
+
+    totalLikes:{
+        type:Number,
+        default:0
+    },
+    totalComments:{
+        type:Number,
+        default:0
+    },
+    followers:{
+        type:Number,
+        default:0
+    },
+    following:{
+        type:Number,
+        default:0
+    },
+
+    // settings for later use user default settings are stored here
+    //settings:{}
+    
+
+},{timestamps:true})
+
+module.exports = mongoose.model('user',UserSchema)
