f406cc2a19abefa1bdf6694ae443878ee243227d
Mohanacgkallagunta <mohanakallagunta@gmail.com>
2026-05-04 11:55:06 +0530

    password routes

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,16 @@
+const nodemailer=require("nodemailer")
+require("dotenv").config()
+
+console.log("---------------------",process.env.EMAIL,process.env.PASSWORD)
+const transporter=nodemailer.createTransport({
+    service:"gmail",
+    host:"smtp.gmail.com",
+    port:465,
+    secure:false,
+    auth:{
+        user:process.env.EMAIL,
+        pass:process.env.PASSWORD 
+    }
+})
+
+module.exports=transporter 
